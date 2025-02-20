be76f5a42ece0af161ed14d813dc0c1da4f2e6a4
Biswarup <biswarupghosh520@gmail.com>
2025-02-20 03:54:11 +0000

    added one clientId validation

--- model/services/validation/validation.js
+++ model/services/validation/validation.js
@@ -1,6 +1,6 @@
 //validation for client id
 const clientIdValidation = ({ clientId }) => {
-    if (!clientId || typeof clientId !== "string" || !/^[A-Za-z0-9]+$/.test(clientId)) {
+    if (!clientId || typeof clientId !== "string" || clientId.length !== 24 || !/^[A-Za-z0-9]+$/.test(clientId)) {
         return { status: false, message: "Some networking problem" };
     }
     return { status: true, message: "Success" }

4570fcd67d73a18b7123c161715dad90eb7c6dc2
PragyaNehaDas <pragyanehadas@gmail.com>
2025-03-04 08:31:03 +0000

    fixed minor changes on logOut functionality

--- controller/user/logOutUser.controller.js
+++ controller/user/logOutUser.controller.js
@@ -1,13 +1,13 @@
+const logOut = require("../../model/services/user/logOut")
+const sanitizeBody = require("../../utils/sanitizeBody")
 
 const logOutUser = async (req, res, next) =>{
     try {
-        const token = req.cookies.token
-        if(!token){
-            return res.status(400).json({status: false, message: "User not logged out successfully"})
-        }
-        
-        await res.clearCookie("token")
-        return res.status(200).json({status: true, message: "User logged out successfully"})
+        const data = await sanitizeBody(req.body)
+        const id = data?.id || data?.userId
+        const {clientId} = data
+        const result = await logOut({clientId, userId: id})
+        return res.status(200).json({status: result?.status, message: result?.message})
     } catch (error) {
         next(error)
     }
--- documentation/logOutUser.js
+++ documentation/logOutUser.js
@@ -1,33 +1,66 @@
-/** 
-* @swagger 
-* /users/logout: 
-*   post: 
-*     summary: Log out the user
-*     description: Logs out the currently authenticated user by invalidating their session or token.
-*     tags:
-*       - Authentication
-*     security:
-*       - BearerAuth: []
-*     responses:
-*       200:
-*         description: User successfully logged out
-*         content:
-*           application/json:
-*             example:
-*               status: true
-*               message: "User logged out successfully"
-*       401:
-*         description: Unauthorized, missing or invalid token
-*         content:
-*           application/json:
-*             example:
-*               status: false
-*               message: "Unauthorized access"
-*       500:
-*         description: Internal server error
-*         content:
-*           application/json:
-*             example:
-*               status: false
-*               message: "Internal server error"
-*/
+/**
+ * @swagger
+ * /users/logout:
+ *   post:
+ *     summary: Log out the user
+ *     description: Logs out the user where in the userId the id of the user need to be given and also the clientId is required.
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userId:
+ *                 type: string
+ *                 description: The ID of the user to log out.
+ *                 example: "67b5cfd46973cf6d93c75503"
+ *               clientId:
+ *                 type: string
+ *                 description: The client ID associated with the request.
+ *                 example: "6788abe40db7c3b61ed93c70"
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       200:
+ *         description: User successfully logged out
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: boolean
+ *                   example: true
+ *                 message:
+ *                   type: string
+ *                   example: "User logged out successfully"
+ *       401:
+ *         description: Unauthorized, missing or invalid token
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: "Unauthorized access"
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: "Internal server error"
+ */

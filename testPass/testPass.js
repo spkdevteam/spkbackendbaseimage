130ae7e32d90670369a9393f3a5b8a4d0665c76c
PragyaNehaDas <pragyanehadas@gmail.com>
2025-03-09 13:11:38 +0000

    changes made

--- testPass/testPass.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-const { encodePassword } = require("../utils/testPassword");
-
-const encodedPassword = encodePassword("Ayan@1997");
-
-console.log("Encoded Password Token:", encodedPassword);

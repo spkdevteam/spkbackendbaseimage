e99ac9b4027969186d274f3d56f37875790ee396
PragyaNehaDas <pragyanehadas@gmail.com>
2025-03-13 05:32:10 +0000

    department added

--- model/services/department/createDepartmentFn.js
+++ model/services/department/createDepartmentFn.js
@@ -1,47 +1,55 @@
-const { default: mongoose } = require("mongoose");
+const mongoose  = require("mongoose");
 const { getClientDatabaseConnection } = require("../../connection");
 const { departmentSchema } = require("../../department");
+
+const {  clientIdValidation, emptyStringValidation, stringValidationWithAll } = require("../validation/validation");
 const getserialNumber = require("../../serialNumber.jss/getSerialNumber");
-const { stringValidation, clientIdValidation, emptyStringValidation } = require("../validation/validation");
 
 const createDepartmentFn = async ({ _id = null, userId, deptName, companyId, description, shift, clientId }) => {
     try {
         //checking the ids
-        switch(true){
+        switch (true) {
             case !mongoose.Types.ObjectId.isValid(userId):
-                return { status: false, message: "Invalid user id, try again"};
+                return { status: false, message: "Invalid user id, try again" };
             case !mongoose.Types.ObjectId.isValid(companyId):
-                return { status: false, message: "Invalid company id, try again"};
+                return { status: false, message: "Invalid company id, try again" };
         };
 
-        if(_id && !mongoose.Types.ObjectId.isValid(_id)){
-            return {status: false, message: "Invalid _id, try again"};
+        if (_id && !mongoose.Types.ObjectId.isValid(_id)) {
+            return { status: false, message: "Invalid _id, try again" };
         };
 
         //validating
         const validation = [
-            stringValidation({ string: deptName, name: "department name: " }),
+            stringValidationWithAll({ string: deptName, name: "department name: " }),
             emptyStringValidation({ string: description, name: "description: " }),
             clientIdValidation({ clientId })
         ];
 
         const error = validation.filter((e) => e && e.status === false);
         if (error.length > 0) return { status: false, message: error.map(e => e.message).join(", ") };
-    
+
         //establishing connection to the db
         const db = await getClientDatabaseConnection(clientId);
         const Department = await db.model("Department", departmentSchema);
+        const alreadyExists = await Department.findOne({deptName: deptName, companyId: companyId})
+        if(alreadyExists){
+            return {status: false, message: "Duplicate deptName cannot be exists with same companyId"}
+        }
+        console.log("first", alreadyExists);
 
         //generating serial number
-        const serialNumber = await getserialNumber("department", clientId, "");
+        const serialNumber = await getserialNumber("department", clientId, "Dept")
+
+        console.log(serialNumber,);
 
         //making it an absolute number
-        const absSerialNumber = Math.abs(serialNumber);
+
 
         //creating new department
         const department = await Department.insertDepartment({
             _id,
-            displayId: absSerialNumber,
+            displayId: serialNumber,
             userId,
             deptName,
             companyId,
@@ -49,11 +57,13 @@ const createDepartmentFn = async ({ _id = null, userId, deptName, companyId, des
             shift
         });
         console.log("====================", department);
-
+        
+        
+        
         //checking wheather the creating department was done or not
-        if(!department?.status) return { status: false, message: "Operation failed try again later"};
+        if (!department?.status) return { status: false, message: "Operation failed try again later" };
         console.log("//////////////////", department.userId);
-        return { status: true, message: "New department is created", data: {_id: department.department._id} };
+        return { status: true, message: "New department is created", data: { _id: department.department._id } };
     } catch (error) {
         return { status: false, message: error.message };
     }
